e0ca4fad883cae8c00690042f663781e5f4140ab
cedric-prunier <cedric-prunier@laplateforme.io>
2023-05-23 11:33:13 +0200

    premier

---
+++ jour02/job05.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    function estPremier($nombre)
+    {
+        if ($nombre < 2) {
+            return false;
+        }
+
+        for ($i = 2; $i <= sqrt($nombre); $i++) {
+            if ($nombre % $i === 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    for ($i = 2; $i <= 1000; $i++) {
+        if (estPremier($i)) {
+            echo $i . "<br />";
+        }
+    }
+    $i = sqrt(2);
+    echo $i;
+    ?>
+
+</body>
+
+</html>
