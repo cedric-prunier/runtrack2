2d3d87c182e691eb8e88b8d215850440af65aac1
cedric-prunier <cedric-prunier@laplateforme.io>
2023-05-24 10:42:10 +0200

    premier

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,8 +1,12 @@
 <?php
-
 $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+$strLength = 0;
 
-for ($i = 0; $i < strlen($str); $i += 2) {
-    echo $str[$i];
+// Parcourir la chaîne caractère par caractère jusqu'à ce que le caractère à l'indice $strLength soit une chaîne vide
+while (isset($str[$strLength])) {
+    if ($strLength % 2 == 0) {
+        echo $str[$strLength];
+    }
+    $strLength++;
 }
 ?>
